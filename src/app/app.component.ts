4ec889789f9bc90dd23cbc100994560387c61c69
Shruti-Lal <shrutilal_99@protonmail.com>
2020-12-08 18:57:26 +0530

    angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo16';
+  myLoginForm:FormGroup;
+  constructor(){
+    this.myLoginForm=new FormGroup(
+      {
+        username:new FormControl('sd',[Validators.required,Validators.minLength(5),Validators.maxLength(8)]),
+        password:new FormControl('sd',[Validators.required,Validators.minLength(4),Validators.maxLength(7)]),
+        gender:new FormControl('',Validators.required),
+        course:new FormControl('',Validators.required),
+        timings:new FormControl('',Validators.required),
+        accept:new FormControl('',Validators.required)
+      }
+    )
+  }
+  public verifyUser(){
+    console.log("verifyUser");
+    console.log("Username-"+this.myLoginForm.controls.username.value);
+    console.log("Password-"+this.myLoginForm.controls.password.value);
+    console.log("Gender-"+this.myLoginForm.controls.gender.value);
+    console.log("Course-"+this.myLoginForm.controls.course.value);
+    console.log("Timings-"+this.myLoginForm.controls.timings.value);
+    console.log("ACCEPT-"+this.myLoginForm.controls.accept.value);
+    this.myLoginForm.reset();
+    console.log("Username-"+this.myLoginForm.controls.username.value);
+    console.log("Passwod-"+this.myLoginForm.controls.password.value);
+    console.log("Gender-"+this.myLoginForm.controls.gender.value);
+    
+    console.log("Course-"+this.myLoginForm.controls.course.value);
+    console.log("Timings-"+this.myLoginForm.controls.timings.value);
+
+  }
 }
